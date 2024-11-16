20d21457ebedcbd8030ff3e9a18eb9cdcdbc2d80
Edem <edem.denkey@myeducoach.de>
2024-11-16 15:58:16 +0100

    added StockMarket class

---
+++ StockMarket.cpp
@@ -0,0 +1,10 @@
+//
+// Created by User on 16.11.2024.
+//
+
+#include "StockMarket.h"
+
+StockMarket::StockMarket(float timeStep, int simulationLength) {
+    this->timeStep = timeStep;
+    this->simulationLength = simulationLength;
+}
