972d2bf62ca87f586c7c760ff5dc4f6dc947eabd
Edem <edem.denkey@myeducoach.de>
2024-11-30 22:44:43 +0100

    Started merging our code

--- src/Stock.cpp
+++ src/Stock.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <math.h>
 #include <random>
+#include <vector>
 
 /* TODO MY QUESTIONS:
  * change timestep do double for consistency
@@ -9,68 +10,44 @@
  * updatePrice right now void --> should we instead return to make recording of the time series easier?
  * which statistical values?
  * what about priceChange?
+ * Stock's name attribute vs. map?
  */
 
 
 // Constructor
 Stock::Stock(double price, double expectedReturn, double variance, double priceChange) :
-    price{price}, expectedReturn{expectedReturn}, variance{variance}, priceChange{priceChange} {}
+    price{price}, expectedReturn{expectedReturn}, variance{variance}, priceChange{priceChange}, priceTimeSeries({price}) {}
 
-double Stock::getPrice() const {
-    return price;
-};
+// Getters
+double Stock::getPrice() const {return price;}
 
-double Stock::getExpectedReturn() const {
-    return expectedReturn;
-};
+double Stock::getExpectedReturn() const {return expectedReturn;}
 
-double Stock::getVariance() const {
-    return variance;
-};
+double Stock::getVariance() const {return variance;}
 
-std::string_view Stock::getName() const {
-    return name;
-};
+std::string_view Stock::getName() const {return name;}
 
-// TODO add setters
+std::vector<double> Stock::getPriceTimeSeries() const {return priceTimeSeries;}
+
+// Setters
+void Stock::saveCurrentPrice(double price) {priceTimeSeries.push_back(price);}
+
+void Stock::setPrice(double price) {this->price = price;}
+
+void Stock::setExpectedReturn(double expectedReturn) {this->expectedReturn = expectedReturn;}
+
+void Stock::setVariance(double variance) {this->variance = variance;}
 
 // generates random number from standard normal distribution
 double drawRandomNumber(std::default_random_engine& generator) {
     double mean{0.0};
     double var{1.0};
-
     std::normal_distribution<double> distribution{mean, sqrt(var)};
     return distribution(generator);
 }
 
 // simulates new price for one timestep with Geometric Brownian Motion (see link in README)
-void Stock::updatePrice(const double& timestep, std::default_random_engine& generator) {
+void Stock::updatePrice(const double& timeStep, std::default_random_engine& generator) {
     double randomNumber = drawRandomNumber(generator);
-    price = price + price * (expectedReturn * timestep + randomNumber * sqrt(variance * timestep));
+    price = price + price * (expectedReturn * timeStep + randomNumber * sqrt(variance * timeStep));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-double Stock::getExpectedReturn() const { //sarper added this, explaiantion is in Stocks.h file
-    return expectedReturn;
-}
-
-void Stock::updatePrice(double newPrice) {  // sarper added this, explanation is in .h file
-    price = newPrice;
-}
-
