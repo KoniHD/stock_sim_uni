bcf5750cbbfbfb7afd1f3a9acdb917d4728cbdf8
Koni <konstantin.zeck+github@gmail.com>
2024-12-22 23:05:15 +0100

    Updated documentation for Wallet class

--- include/Wallet.h
+++ include/Wallet.h
@@ -13,44 +13,60 @@ class Wallet {
 
     double _funds;
     double _cash_position;
+
+    /**
+     * @brief The Strategy used for picking the stocks in @link _portfolio @endlink.
+     *
+     * The Strategy is unique to this Wallet. It can be implemented with low or high risk.
+     */
     std::unique_ptr<Strategy> _strategy;
+
+    /**
+     * @brief The Stockmarket at which the Stocks in the @link _portfolio @endlink are listed.
+     */
     std::shared_ptr<StockMarket> _market;
+
+    /**
+     * @brief The portfolio of Stocks owned by the Wallet.
+     */
     std::unordered_map<std::string, int> _portfolio;
 
 public:
     Wallet() = delete;
 
     /**
-     * @brief Construct a new Wallet object and initialize the funds, strategy and market.
+     * @brief Construct a new Wallet object and initialize the @link _funds @endlink, @link _strategy @endlink and @link
+     * _market @endlink.
      */
     Wallet(double, std::unique_ptr<Strategy>, std::shared_ptr<StockMarket>);
 
     /**
-     * @brief Checks whether the wallet contains the @link{Stock} specified by name
-     * @return Whether the Wallet contains the @link{Stock}
+     * @brief Checks whether the Wallet contains the Stock specified by @p stock_name
+     * @param stock_name The name of the Stock to check.
+     * @return Whether the Wallet contains the Stock
      */
-    bool containsStock(std::string_view) const noexcept;
+    bool containsStock(std::string_view stock_name) const noexcept;
 
     /**
-     * @brief Get the funds of the wallet.
-     * @return The funds of the wallet.
+     * @brief Get the @link _funds @endlink of the Wallet.
+     * @return The @link _funds @endlink of the Wallet.
      */
     [[nodiscard]] double getFunds() const;
 
     /**
-     * @brief Allow the user to buy a specified stock.
-     * @param[0] The stock to buy
-     * @param[1] The amount of stocks to be bought
+     * @brief Allow the user to buy a specified Stock.
+     * @param stock The Stock to buy
+     * @param amount The amount of Stocks to be bought
      * @return Whether the purchase was succesful.
      *
-     * The method attemts to buy the amount of specified stocks using money from the @link{_cash_position}. If there is
-     * not enough money in @link{_cash_position} and error is printed and no stocks are bought.
+     * The method attempts to buy stocks (specified by @p amount) using money from the @link _cash_position @endlink. If
+     * there is not enough money in @link _cash_position @endlink and error is printed and no Stocks are bought.
      */
     bool buyStocks(const Stock &, unsigned) noexcept;
 
     /**
-     * @brief Prints _cashPosition, each owned stock (value in owned stock, number stocks), and overall portfolio Value
-     * and performance.
+     * @brief Prints @link _cashPosition @endlink, each owned stock (value in owned stock, number stocks), and overall
+     * portfolio value and performance.
      *
      * The function prints values rounded to two significant digits after dot for cash position, and the performance.
      * The other values are printed in their stored precision.
@@ -58,9 +74,10 @@ public:
     void printWalletInfo();
 
     /**
-     * @brief Calculate the total value of the portfolio.
+     * @brief Calculate the total value of the @link _portfolio @endlink.
      *
-     * This function adds together the value of each stock contained in the portfolio times its current price.
+     * This function adds together the value of each Stock contained in the @link _portfolio @endlink times its current
+     * price.
      */
     void evaluateResults();
 };
