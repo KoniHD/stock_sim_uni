8eca88adcd35ca1364727ffb5cbc881a3efc7a81
Koni <konstantin.zeck+github@gmail.com>
2025-01-26 18:57:57 +0100

    Adding a profiling executable

--- tests/profiling.cpp
+++ tests/profiling.cpp
@@ -5,34 +5,35 @@
  * 2. Create 4 wallets (the maximal number we allow in client.cpp)
  * 2.1 Wallet 1: 3'000 funds, low risk strategy, Wallet 2: 10'000 funds, high risk strategy, Wallet 3: 3'000 funds, low
  * risk strategy, Wallet 4: 10'000 funds, high risk strategy
- * 3. Run the simulation for 100 months.
+ * 3. Run the simulation for 10'000 months.
  * 4. Print the performance of each wallet.
- * 5.1 Extend the simulation for 50 months.
+ * 5.1 Extend the simulation for 5'000 months.
  * 5.2 Sell 1 "Google" stock from wallet 1.
  * 6. Finish the simulation.
  */
 
 #include "HighRiskStrategy.h"
 #include "LowRiskStrategy.h"
+#include "Stock.h"
 #include "StockMarket.h"
-#include "Strategy.h"
 #include "Wallet.h"
 
 #include <cstddef>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <vector>
 
 int main()
 {
 
-    std::shared_ptr<StockMarket> market = std::make_shared<StockMarket>(1.0 / 12.0, 100, "../input/stocks.json");
+    std::shared_ptr<StockMarket> market = std::make_shared<StockMarket>(1.0 / 12.0, 10'000, "../input/stocks.json");
 
     std::vector<Wallet> wallets;
-    wallets.emplace_back(Wallet(3000, std::make_unique<LowRiskStrategy>(), market));
-    wallets.emplace_back(Wallet(10'000, std::make_unique<LowRiskStrategy>(), market));
-    wallets.emplace_back(Wallet(3'000, std::make_unique<HighRiskStrategy>(), market));
-    wallets.emplace_back(Wallet(10'000, std::make_unique<HighRiskStrategy>(), market));
+    wallets.emplace_back(3000, std::make_unique<LowRiskStrategy>(), market);
+    wallets.emplace_back(10'000, std::make_unique<LowRiskStrategy>(), market);
+    wallets.emplace_back(3'000, std::make_unique<HighRiskStrategy>(), market);
+    wallets.emplace_back(10'000, std::make_unique<HighRiskStrategy>(), market);
 
     market->simulateMarket();
 
@@ -41,7 +42,7 @@ int main()
         wallets.at(i).printWalletInfo();
     }
 
-    market->setSimulationLength(50);
+    market->setSimulationLength(5'000);
 
     Stock google;
     if (wallets.at(0).containsStock("Google")) {
@@ -62,9 +63,9 @@ int main()
 
     size_t i{0};
     for (const Wallet &wallet: wallets) {
-        std::cout << std::endl << "-Stats for wallet No." << std::to_string(++i) << " :" << std::endl;
-        wallet.getPortfolioValue();
-        wallet.getFunds();
+        std::cout << std::endl
+                  << "-Stats for wallet No." << std::to_string(++i) << " :" << wallet.getPortfolioValue()
+                  << wallet.getFunds() << std::endl;
     }
 
     market->outputPerformance();
