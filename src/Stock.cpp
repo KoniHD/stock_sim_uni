0c78b86414eb56d0609ed26728a4fd54a95d9aeb
Koni <konstantin.zeck+github@gmail.com>
2025-01-27 00:21:55 +0100

    Fix #30 Faulty error message in stock constructor

--- src/Stock.cpp
+++ src/Stock.cpp
@@ -20,17 +20,17 @@ Stock::Stock(std::string name, double price, double expectedReturn, double stand
     marketCap{availableStocks * price},
     sellExecuted{false},
     buyExecuted{false},
-    orderVolume{0.0} {
-    if (price < 0) throw std::invalid_argument("Price must be non-negative");
-    if (expectedReturn < 0 || expectedReturn > 0.5) throw std::invalid_argument("Expected return must be between 0 and 0.2");
-    if (standardDev < 0 || standardDev > 0.5) throw std::invalid_argument("standardDev must be between 0 and 0.5");
-    if (availableStocks < 0) throw std::invalid_argument("Available stocks must be non-negative");}
-
-Stock::Stock() :
-    price{0.0},
-    expectedReturn{0.0},
-    standardDev{0.0}
-{}
+    orderVolume{0.0}
+{
+    if (price < 0)
+        throw std::invalid_argument("Price must be non-negative");
+    if (expectedReturn < 0 || expectedReturn > 0.5)
+        throw std::invalid_argument("Expected return must be between 0 and 0.5");
+    if (standardDev < 0 || standardDev > 0.5)
+        throw std::invalid_argument("standardDev must be between 0 and 0.5");
+    if (availableStocks < 0)
+        throw std::invalid_argument("Available stocks must be non-negative");
+}
 
 // Getters
 double Stock::getPrice() const { return price; }
@@ -43,12 +43,11 @@ std::string_view Stock::getName() const { return name; }
 
 std::vector<double> Stock::getPriceTimeSeries() const { return priceTimeSeries; }
 
-bool Stock::getSellExecuted() const {return sellExecuted; }
+bool Stock::getSellExecuted() const { return sellExecuted; }
 
-bool Stock::getBuyExecuted() const {return buyExecuted; }
+bool Stock::getBuyExecuted() const { return buyExecuted; }
 
 double Stock::getOrderVolume() const noexcept { return orderVolume; }
-
 
 // Setters
 void Stock::saveCurrentPrice(double price) { priceTimeSeries.push_back(price); }
@@ -61,12 +60,11 @@ void Stock::setStandardDev(double standardDev) { this->standardDev = standardDev
 
 void Stock::setName(std::string_view name) { this->name = name; }
 
-void Stock::setSellExecuted(bool state) {this -> sellExecuted = state;}
+void Stock::setSellExecuted(bool state) { this->sellExecuted = state; }
 
-void Stock::setBuyExecuted(bool state) {this -> buyExecuted = state;}
+void Stock::setBuyExecuted(bool state) { this->buyExecuted = state; }
 
-void Stock::setOrderVolume(double orderVolume) { this -> orderVolume = orderVolume; }
-
+void Stock::setOrderVolume(double orderVolume) { this->orderVolume = orderVolume; }
 
 // generates random number from standard normal distribution
 double drawRandomNumber(std::default_random_engine &generator)
@@ -77,34 +75,32 @@ double drawRandomNumber(std::default_random_engine &generator)
     return distribution(generator);
 }
 
-
 /**
-* @brief Update prices accoriding to Geometric Brownian Motion (cf. README), taking buys and sells into account
-* @param &timeStep value between 0 and 1 determining the time step size of the simulation (e.g. 1/12 for monthly steps)
-* @param &generator Random generator responsible for the stochastics of the simulation
-*
-* The method is applied to a single stock and calculates the new price for a simulation time step according to a
-* Geometric Brownian Motion. If a buy (or sell) has been executed, the expected return gets positively (or negatively)
-* affected, proportionally to the relation between order Volume and the available stocks. Additionally, the standardDev
-* , i.e. the volatility of the stock is enhanced by 20%. More details can be found in the README section concerning
-* sprint 2.
-*/
+ * @brief Update prices accoriding to Geometric Brownian Motion (cf. README), taking buys and sells into account
+ * @param &timeStep value between 0 and 1 determining the time step size of the simulation (e.g. 1/12 for monthly steps)
+ * @param &generator Random generator responsible for the stochastics of the simulation
+ *
+ * The method is applied to a single stock and calculates the new price for a simulation time step according to a
+ * Geometric Brownian Motion. If a buy (or sell) has been executed, the expected return gets positively (or negatively)
+ * affected, proportionally to the relation between order Volume and the available stocks. Additionally, the standardDev
+ * , i.e. the volatility of the stock is enhanced by 20%. More details can be found in the README section concerning
+ * sprint 2.
+ */
 void Stock::updatePrice(const double &timeStep, std::default_random_engine &generator)
 {
     double tradeImpactExp{0.0};
     double tradeImpactStandardDev{1.0};
-    if(buyExecuted == true) {
-        tradeImpactExp = 100 * standardDev * orderVolume/availableStocks; // 100 is empirical value
-        tradeImpactStandardDev = 150 * orderVolume/availableStocks; // 150 is empirical value
+    if (buyExecuted == true) {
+        tradeImpactExp         = 100 * standardDev * orderVolume / availableStocks; // 100 is empirical value
+        tradeImpactStandardDev = 150 * orderVolume / availableStocks; // 150 is empirical value
     } else if (sellExecuted == true) {
-        tradeImpactExp = -100 * standardDev * orderVolume/availableStocks;
-        tradeImpactStandardDev = 150 * orderVolume/availableStocks;
+        tradeImpactExp         = -100 * standardDev * orderVolume / availableStocks;
+        tradeImpactStandardDev = 150 * orderVolume / availableStocks;
     }
 
     double randomNumber = drawRandomNumber(generator);
-    price               = price + price * ((expectedReturn + tradeImpactExp) * timeStep + randomNumber * standardDev * tradeImpactStandardDev * sqrt(timeStep));
+    price               = price + price * ((expectedReturn + tradeImpactExp) * timeStep +
+                             randomNumber * standardDev * tradeImpactStandardDev * sqrt(timeStep));
     price               = std::max(price, 0.01);
     priceTimeSeries.push_back(price);
 }
-
-
